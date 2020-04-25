d0f229a7c94ad29c34957b1eb5186faa3b4454a2
Ximenez Rodrigues Alves Bruno <bximenez@protonmail.com>
2020-04-25 15:58:58 +0200

    Wrote a bit about the results obtained in rotating wave section in the context of optical clocks. Calculated the approximate laser line width based on the duration of the laser pulse. Related the result with the coherence time of the laser.

--- Manuscript/plots/Rabi_Oscillations/hwhm_calc.py
+++ Manuscript/plots/Rabi_Oscillations/hwhm_calc.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+'''
+Solving t_pi x rabi width from the notes
+'''
 c0 = np.sqrt(2)/np.pi - 1
 c1 = 0
 c2 = 1/6
@@ -16,4 +18,20 @@ for s in solutions:
         x = np.real(s) #  There are two solution whose abs value is equal
 
 t_sigma = np.sqrt( np.square(2*x/np.pi) - 1 )
+print(t_sigma)
+
+'''
+Now for the fourier spectrum of the laser
+'''
+
+c0 = -1/2
+coeff = [c6, c5, c4, c3, c2, c1, c0]
+solutions = np.roots(coeff)
+
+for s in solutions:
+    if np.imag(s) == 0:
+        x = np.real(s) #  There are two solution whose abs value is equal
+
+t_pi = 0.2
+t_sigma = 4*x/(t_pi*2*np.pi)
 print(t_sigma)
